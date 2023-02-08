0a26bc7e31bebba498eabe38437711e8f0c5a615
Sutardi Hardiansyah <sutardi@xeranta.com>
2023-02-08 09:42:13 +0700

    Update new

--- src/Manga.js
+++ src/Manga.js
@@ -1,98 +1,122 @@
-import { useEffect, useState } from 'react'
-import { useParams, useNavigate } from "react-router-dom"
-import axios from 'axios'
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Manga = () => {
-    const {page} = useParams()
-    const navigate = useNavigate()
-    const [isLoading, setIsLoading] = useState(true)
-    const oldUrl = "http://localhost:5000/manga";
-    let url = page ? oldUrl+'/page/' +page : oldUrl;
-    const [next, setNext] = useState("");
-    const [prev, setPrev] = useState("");
-    const [mangaList, setMangaList] = useState([]);
+  const { page } = useParams();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const oldUrl = "http://localhost:5000/manga";
+  let url = page ? oldUrl + "/page/" + page : oldUrl;
+  const [next, setNext] = useState("");
+  const [prev, setPrev] = useState("");
+  const [mangaList, setMangaList] = useState([]);
 
-    const getData = async (link = url) => {
-        const response = await axios.get(link);
-        setMangaList(response.data.manga_list)
-        setNext(response.data.next);
-        setPrev(response.data.prev);
-    }
+  const getData = async (link = url) => {
+    const response = await axios.get(link);
+    setMangaList(response.data.manga_list);
+    setNext(response.data.next);
+    setPrev(response.data.prev);
+  };
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-        getData()
-    }, [page])
-    
-    const btnNext = (page) => {
-        setIsLoading(true);
-        navigate(`/manga/page/${page}`)
-    }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    getData();
+  }, [page]);
 
-    const btnPrev = (page) => {
-        setIsLoading(true);
-        console.log(page);
-        let url = page == 1 ? '/' :  `/manga/page/${page}`
-        navigate(url)
-    }
-    
-    return (
-        <div className="pt-24 pb-7">
-            <div className="container px-4 mx-auto">
-                
-                <div className="grid grid-cols-12 mb-10">
+  const btnNext = (page) => {
+    setIsLoading(true);
+    navigate(`/manga/page/${page}`);
+  };
 
-                    <div className="col-span-8">
-                        <div className="grid grid-cols-1 2sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 2sm:gap-x-36 gap-y-24 sm:gap-x-52 md:gap-y-44 lg:gap-y-20">
-                            {
-                                mangaList.map( (manga, i) => (
-                                        <>
-                                        <div key={i} className="w-[161px] h-[325px] xs:w-[193px] sm:w-[240px] md:w-80 lg:w-60">
-                                            <Link to={`/manga/detail/${manga.endpoint}`} className="group relative block" title={manga.title} key={i}>
-                                                {/* <a href={`/manga/detail/${manga.endpoint}`}>Detail</a>
+  const btnPrev = (page) => {
+    setIsLoading(true);
+    console.log(page);
+    let url = page == 1 ? "/" : `/manga/page/${page}`;
+    navigate(url);
+  };
+  const searchData = async (e) => {
+    const res = await axios.get(`http://localhost:5000/manga/search/${e}`);
+    console.log(res);
+  };
+  return (
+    <div className="pt-24 pb-7">
+      <div className="container px-4 mx-auto">
+        <input
+          type="text"
+          placeholder="seacrh"
+          onChange={(e) => searchData(e.target.value)}
+        />
+        <div className="grid grid-cols-12 mb-10">
+          <div className="col-span-8">
+            <div className="grid grid-cols-1 2sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 2sm:gap-x-36 gap-y-24 sm:gap-x-52 md:gap-y-44 lg:gap-y-20">
+              {mangaList.map((manga, i) => (
+                <>
+                  <div
+                    key={i}
+                    className="w-[161px] h-[325px] xs:w-[193px] sm:w-[240px] md:w-80 lg:w-60"
+                  >
+                    <Link
+                      to={`/manga/detail/${manga.endpoint}`}
+                      className="group relative block"
+                      title={manga.title}
+                      key={i}
+                    >
+                      {/* <a href={`/manga/detail/${manga.endpoint}`}>Detail</a>
                                                 <p>{ manga.title }</p> */}
-                                                {/* <div style={{backgroundImage: `url(${manga.thumb})`}} className=""></div> */}
-                                                <img src={`${manga.thumb}`} alt="" className="w-full rounded-md" />
-                                                <div className="absolute bottom-0 w-full h-16 manga-title transition-all duration-500 group-hover:h-24">
-                                                    <h4 className="text-white overflow-ellipsis overflow-hidden whitespace-nowrap font-bold">{ manga.title }</h4>
-                                                </div>
-                                            </Link>
+                      {/* <div style={{backgroundImage: `url(${manga.thumb})`}} className=""></div> */}
+                      <img
+                        src={`${manga.thumb}`}
+                        alt=""
+                        className="w-full rounded-md"
+                      />
+                      <div className="absolute bottom-0 w-full h-16 manga-title transition-all duration-500 group-hover:h-24">
+                        <h4 className="text-white overflow-ellipsis overflow-hidden whitespace-nowrap font-bold">
+                          {manga.title}
+                        </h4>
+                      </div>
+                    </Link>
 
-                                            <Link className="text-white block rounded-bl-md rounded-br-md bg-black h-[80px] border-t-2 border-white hover:bg-sky-700" to={`/manga/chapter/${manga.chapter_endpoint}`}>
-                                                <div className="flex justify-between text-sm">
-                                                    <span className="">#{manga.chapter_number}</span>
-                                                    <span className="text-right">{manga.updated_on}</span>
-
-                                                </div>
-                                                <p className="text-gray-200 mt-3">{ manga.chapter }</p>
-                                            </Link>
-                                        </div>
-                                        </>
-                                       
-                                    
-                                ))
-                            }
-
-                        </div>
-
-                    </div>
-                    <div className="col-span-8">
-                        
-                    </div>
-                </div>
-                
-                <div className="">
-                    {
-                        prev === "" ?
-                        "" :
-                        <button onClick={() => btnPrev(`${prev}`)} className="text-white bg-sky-700 px-5 py-2 rounded-md hover:bg-sky-800">Prev</button>
-                    }
-                    <button onClick={() => btnNext(`${next}`)} className="text-white bg-sky-700 px-5 py-2 rounded-md hover:bg-sky-800">Next</button>
-                </div>
+                    <Link
+                      className="text-white block rounded-bl-md rounded-br-md bg-black h-[80px] border-t-2 border-white hover:bg-sky-700"
+                      to={`/manga/chapter/${manga.chapter_endpoint}`}
+                    >
+                      <div className="flex justify-between text-sm">
+                        <span className="">#{manga.chapter_number}</span>
+                        <span className="text-right">{manga.updated_on}</span>
+                      </div>
+                      <p className="text-gray-200 mt-3">{manga.chapter}</p>
+                    </Link>
+                  </div>
+                </>
+              ))}
             </div>
+          </div>
+          <div className="col-span-8"></div>
         </div>
-    )
-}
 
-export default Manga
+        <div className="">
+          {prev === "" ? (
+            ""
+          ) : (
+            <button
+              onClick={() => btnPrev(`${prev}`)}
+              className="text-white bg-sky-700 px-5 py-2 rounded-md hover:bg-sky-800"
+            >
+              Prev
+            </button>
+          )}
+          <button
+            onClick={() => btnNext(`${next}`)}
+            className="text-white bg-sky-700 px-5 py-2 rounded-md hover:bg-sky-800"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Manga;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,109 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [dataSearch, setDataSearch] = useState([]);
+  const searchData = async (e) => {
+    const res = await axios.get(`http://localhost:5000/manga/search/${e}`);
+    console.log(res.data);
+    setDataSearch(res.data.manga_list);
+  };
   return (
     <header className="bg-[#070720] w-full absolute top-0 flex left-0 items-center z-10 text-white">
-        <div className="container">
-            <div className="flex items-center justify-between relative">
-                <div className="px-4">
-                    <p className="py-6 font-bold text-blue-400 block text-lg">Logo</p>
-                </div>
-                <div className="flex items-center px-4">
-                    {/* <button id="hamburger" type="button" className="block absolute right-4 lg:hidden">
+      <div className="container">
+        <div className="flex items-center justify-between relative">
+          <div className="px-4">
+            <p className="py-6 font-bold text-blue-400 block text-lg">Logo</p>
+          </div>
+          <div className="flex items-center px-4">
+            {/* <button id="hamburger" type="button" className="block absolute right-4 lg:hidden">
                         <span className="hamburger-line"></span>
                         <span className="hamburger-line"></span>
                         <span className="hamburger-line"></span>
                     </button> */}
 
-                    <nav className="absolute hidden lg:block py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
-                        <ul className="block lg:flex">
-                            <li className="group">
-                                <Link to="/" className="text-base text-dark py-2 mx-8">Home</Link>
-                            </li>
-                            <li className="group">
-                                <Link to="/anime" className="text-base text-dark py-2 mx-8">Anime</Link>
-                            </li>
-                            <li className="group">
-                                <Link to="/" className="text-base text-dark py-2 mx-8">Manhua</Link>
-                            </li>
-                            <li className="group">
-                                <Link to="/" className="text-base text-dark py-2 mx-8">Manhwa</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+            <nav className="absolute hidden lg:block py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+              <ul className="block lg:flex relative">
+                <li className="group">
+                  <Link to="/" className="text-base text-dark py-2 mx-8">
+                    Home
+                  </Link>
+                </li>
+                <li className="group">
+                  <Link to="/anime" className="text-base text-dark py-2 mx-8">
+                    Anime
+                  </Link>
+                </li>
+                <li className="group">
+                  <Link to="/" className="text-base text-dark py-2 mx-8">
+                    Manhua
+                  </Link>
+                </li>
+                <li className="group">
+                  <Link to="/" className="text-base text-dark py-2 mx-8">
+                    Manhwa
+                  </Link>
+                </li>
+                <li className="group">
+                  <label
+                    for="default-search"
+                    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+                  >
+                    Search
+                  </label>
+                  <div className="relative">
+                    <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <input
+                      type="search"
+                      id="default-search"
+                      className="block py-2 px-12 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Search"
+                      onChange={(e) => searchData(e.target.value)}
+                    />
+                  </div>
+                  <ul className="absolute bg-blue-900 w-[15.5rem] mt-2 rounded-lg h-80 overflow-y-scroll">
+                    {dataSearch.length > 0 &&
+                      dataSearch.map((data) => (
+                        <li className="border-b-[1px] border-b-sky-200 px-2 py-2">
+                          <div className="grid grid-rows-4 grid-flow-col">
+                            <div>
+                              <img
+                                src={`${data.thumb}`}
+                                alt=""
+                                className="h-20"
+                              />
+                            </div>
+                            <p>{data.title}</p>
+                          </div>
+                        </li>
+                      ))}
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
